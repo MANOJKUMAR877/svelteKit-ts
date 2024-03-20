393706dbe8eb0c81a0cb77f9d5ac92ede7d20598
Manojkumar Krishnaramanujam Baskaran <58929289+MANOJKUMAR877@users.noreply.github.com>
2024-03-20 21:21:34 +0100

    use the value of other file post method

---
+++ src/routes/auth-nav/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    // @ts-nocheck
+    
+        import { json } from '@sveltejs/kit';
+    // @ts-nocheck
+    //    let username= ""
+    //    let password=""
+    //    let message=""
+    // 	const HandleSubmit =async (/** @type {undefined} */ e) => {
+    // 		message=""
+    //         const response = await fetch("/auth",{
+    //             method:"POST",
+    //             body:JSON.stringify({
+    //                 username,password
+    //             })
+    //         })
+    //         const data = await response.json()
+    //         message=data.message
+    // 	};
+    export let form;
+    
+    </script>
+    
+    <p>{form?.message||""}</p>
+    <!-- //on:submit|preventDefault={HandleSubmit} -->
+    <form  method="post" action="/auth?/login">
+        <input name="username" />
+        <input name="password" />
+        <button>Login</button>
+        <button formaction="/auth?/register">Register</button>
+    </form>
+    
